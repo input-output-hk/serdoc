c692295c22d93679e090d5978e11c2d9703085eb
Tobias Dammers <tdammers@gmail.com>
2023-12-01 09:15:55 +0100

    Cleanup

--- serdoc-core/src/Data/SerDoc/TestUtil.hs
+++ serdoc-core/src/Data/SerDoc/TestUtil.hs
@@ -1,27 +1,11 @@
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Data.SerDoc.TestUtil
 where
 
-import Test.Tasty.QuickCheck
-import Data.Proxy
-import Control.Monad.Identity
 import Text.Read
-import Control.Monad.Except
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
 import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import Data.Maybe
-
-import Data.SerDoc.Class
-import Data.SerDoc.Info
 
 showBS :: Show a => a -> ByteString
 showBS = encodeUtf8 . Text.pack . show
